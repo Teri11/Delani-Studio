52b6ad76181f909049dd21d0816acec68a9c08cb
Teri11 <augustineteri157@gmail.com>
2021-12-30 14:47:13 +0300

    added portfolio hover

--- script.js
+++ script.js
@@ -22,66 +22,16 @@
 
 
 
-
-
 // PORTFOLIO.....
-$(document).ready(function(){
-  $("#work1").hover(function(){
-  $(".work" ,this).slideToggle(100, "linear").display(100, "linear");
-   }, function(){
-  $(".work" ,this).slideToggle(100, "linear").display(100, "linear");
-  });
-});
-$(document).ready(function(){
-  $("#work2").hover(function(){
-  $(".work" ,this).slideToggle(100, "linear").display(100, "linear");
-   }, function(){
-  $(".work" ,this).slideToggle(100, "linear").display(100, "linear");
-  });
-});
-$(document).ready(function(){
-  $("#work3").hover(function(){
+$(document).ready(function() {
+  $("#work1,#work2,#work3,#work4,#work5,#work6,#work7,#work8").hover(function(){
   $(".work" ,this).slideToggle(100, "linear").display(100, "linear");
    }, function(){
   $(".work" ,this).slideToggle(100, "linear").display(100, "linear");
   });
 });
 
-$(document).ready(function(){
-  $("#work4").hover(function(){
-  $(".work" ,this).slideToggle(100, "linear").display(100, "linear");
-   }, function(){
-  $(".work" ,this).slideToggle(100, "linear").display(100, "linear");
-  });
-});
-$(document).ready(function(){
-  $("#work5").hover(function(){
-  $(".work" ,this).slideToggle(100, "linear").display(100, "linear");
-   }, function(){
-  $(".work" ,this).slideToggle(100, "linear").display(100, "linear");
-  });
-});
-$(document).ready(function(){
-  $("#work6").hover(function(){
-  $(".work" ,this).slideToggle(100, "linear").display(100, "linear");
-   }, function(){
-  $(".work" ,this).slideToggle(100, "linear").display(100, "linear");
-  });
-});
-$(document).ready(function(){
-  $("#work7").hover(function(){
-  $(".work" ,this).slideToggle(100, "linear").display(100, "linear");
-   }, function(){
-  $(".work" ,this).slideToggle(100, "linear").display(100, "linear");
-  });
-});
-$(document).ready(function(){
-  $("#work8").hover(function(){
-  $(".work" ,this).slideToggle(100, "linear").display(100, "linear");
-   }, function(){
-  $(".work" ,this).slideToggle(100, "linear").display(100, "linear");
-  });
-});
+
 // End of portfolio......
 
 
@@ -93,7 +43,7 @@ $(document).ready(function(){
             var message = $(".form-control").val()
             var name = $("#namee").val()
             var email = $("#emaill").val()
-    alert("Hello "+name+",\nWe have received your message from "+email+",\nThank you for contacting us.")
+    alert("Hello" + name + ",\nWe have received your message from: "+ email + ",\nThank you for contacting us.")
     });
     
     // End of contact...
